d6790e669c4bda248e2e460ec72923524dc12cfe
“LoveEleve” <2055384004@qq.com>
2025-12-26 14:56:03 +0800

    gg

--- java/org/apache/tomcat/util/collections/SynchronizedQueue.java
+++ java/org/apache/tomcat/util/collections/SynchronizedQueue.java
@@ -56,7 +56,7 @@ public class SynchronizedQueue<T> {
     public synchronized boolean offer(T t) {
         queue[insert++] = t;
 
-        // Wrap 到达数组末尾,下一次在0位置插入 good test oo pp kk ee ff
+        // Wrap 到达数组末尾,下一次在0位置插入 good test oo pp kk ee ff gg
         if (insert == size) {
             insert = 0;
         }
